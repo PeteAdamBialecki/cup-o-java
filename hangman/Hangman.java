606dea29e4eeaab68ed5d78261703588956028aa
pbialecki_augeofi <pbialecki@augeofi.com>
2019-11-23 12:26:09 -0600

    Terminal prompt guessing.

--- hangman/Hangman.java
+++ hangman/Hangman.java
@@ -2,5 +2,12 @@
 public class Hangman {
     public static void main(String[] args) {
         Game game = new Game("test");
+        Prompter prompter = new Prompter(game);
+        boolean isHit = prompter.promptForGuess();
+        if (isHit) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Oops, wrong.");
+        }
     }
 }
