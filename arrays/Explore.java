6b0ab99298a28661b1fc5429fdc34d6edb70508b
pbialecki_augeofi <pbialecki@augeofi.com>
2019-12-04 20:08:59 -0600

    Unenhanced for-loop sample.

--- arrays/Explore.java
+++ arrays/Explore.java
@@ -1,9 +1,22 @@
 public class Explore {
     public static void main(String[] args) {
         String[] friends = {"Ben", "Alena", "Rick"};
-        for (String friend : friends) {
-            System.out.printf("Hey %s! The movie starts in 30 minutes. See you soon! %n",
-                                friend);
+        // Enhanced for-loop
+        // for (String friend : friends) {
+        //     System.out.printf("Hey %s! The movie starts in 30 minutes. See you soon! %n",
+        //                         friend);
+        // }
+        // // Unenhanced for-loop
+        // for (int i = 0; i < friends.length; i++) {
+        //     String friend = friends[i];
+        //     System.out.printf("Hey %s! The movie starts in 5 minutes! Get over here! %n",
+        //                         friend);
+        // }
+        int[] bensScoreCard = {1, 2, 1, 5, 2, 4, 4, 4, 3, 6, 1, 2, 5, 4, 3, 2, 6, 3};
+        for (int i = 0; i < bensScoreCard.length; i++) {
+            System.out.printf("Hole #%d: %d %n",
+                        i + 1,
+                        bensScoreCard[i]);
         }
     }
 }
