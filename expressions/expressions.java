e01b1fe56c4993a89fba18e804b30c8c60827659
pbialecki_augeofi <pbialecki@augeofi.com>
2020-01-12 14:05:35 -0600

    Zipcode validator.

---
+++ expressions/expressions.java
@@ -0,0 +1,14 @@
+import java.io.Console;
+
+class expressions {
+  public static void main(String[] args) {
+    Console console = System.console();
+    // Your amazing code below
+    String zipCode = console.readLine("Enter your zipcode: ");
+    if (zipCode.matches("\\d{5}")) {
+        System.out.printf("%s is a valid zip code%n", zipCode);
+    } else {
+        System.out.printf("%s is NOT a valid zip code%n", zipCode);
+    }
+  }
+}
