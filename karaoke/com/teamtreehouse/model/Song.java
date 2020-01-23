b3c02897983f526cc470957cdb962ca0c5216ffe
pbialecki_augeofi <pbialecki@augeofi.com>
2020-01-23 17:26:52 -0600

    Song request class.

--- karaoke/com/teamtreehouse/model/Song.java
+++ karaoke/com/teamtreehouse/model/Song.java
@@ -22,8 +22,21 @@ public class Song {
   public String getVideoUrl() {
     return mVideoUrl;
   }
-  
-  
+
+  public boolean equals(Object object) {
+    if (this == object) return true;
+    if (object == null || getClass() != object.getClass()) return false;
+    if (!super.equals(object)) return false;
+    Song song = (Song) object;
+    return java.util.Objects.equals(mArtist, song.mArtist) &&
+            java.util.Objects.equals(mTitle, song.mTitle) &&
+            java.util.Objects.equals(mVideoUrl, song.mVideoUrl);
+  }
+
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), mArtist, mTitle, mVideoUrl);
+  }
+
   @Override
   public String toString() {
     return String.format("Song:  %s by %s", mTitle, mArtist);
