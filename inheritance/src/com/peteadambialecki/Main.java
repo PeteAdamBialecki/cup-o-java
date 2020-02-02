ea451e46ab7b8d25b3a2b475c2359c613c2e5b0f
pbialecki_augeofi <pbialecki@augeofi.com>
2020-02-02 15:46:18 -0600

    Sample object equality.

--- inheritance/src/com/peteadambialecki/Main.java
+++ inheritance/src/com/peteadambialecki/Main.java
@@ -1,19 +1,28 @@
 package com.peteadambialecki;
 
+import java.util.Objects;
+
 public class Main {
 
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.makeSound();
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        System.out.println(dog1.equals(dog2));
     }
 }
 
-class Animal {
+abstract class Animal {
     String sound = "";
 
     Animal(String sound) {
         this.sound = sound;
     }
+
+    public Animal() {
+
+    }
+
+    abstract void findFood();
 
     void makeSound() {
         System.out.println(sound);
@@ -23,11 +32,33 @@ class Animal {
     public String toString() {
         return getClass().getSimpleName() + ": sound = " + sound;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(sound, animal.sound);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sound);
+    }
 }
 
 class Dog extends Animal {
-    Dog() {
-        super( sound: "bark");
+    Dog(String sound) {
+        super(sound);
+    }
+
+    public Dog() {
+
+    }
+
+    @Override
+    void findFood() {
+        System.out.println("looks at human");
     }
 
     @Override
