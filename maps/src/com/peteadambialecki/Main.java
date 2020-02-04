efd4fda71c98c78283e9a4fe7ca46f561e3f020c
pbialecki_augeofi <pbialecki@augeofi.com>
2020-02-04 16:42:29 -0600

    Updated notes and code on maps.

--- maps/src/com/peteadambialecki/Main.java
+++ maps/src/com/peteadambialecki/Main.java
@@ -5,12 +5,23 @@ import java.util.Map;
 
 public class Main {
 
+    public static final String BREAKFAST = "breakfast";
+    public static final String LUNCH = "lunch";
+    public static final String DINNER = "dinner";
+
     public static void main(String[] args) {
         Map<String, String> meals = new HashMap<>();
-        meals.put("breakfast", "waffles");
-        meals.put("lunch", "gyros");
-        meals.put("dinner", "enchiladas");
+        meals.put(BREAKFAST, "waffles");
+        meals.put(LUNCH, "gyros");
+        meals.put(DINNER, "enchiladas");
 
-        System.out.println(meals);
+        System.out.println(meals.get(DINNER));
+
+        String lunch = meals.remove(LUNCH);
+        boolean hasLunch = meals.containsKey(LUNCH);
+        boolean hasGyros = meals.containsValue("Gyro");
+        int size = meals.size();
+
+        System.out.println(lunch + " " + hasLunch + " " + hasGyros + " " + size);
     }
 }
