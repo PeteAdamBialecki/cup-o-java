9982aea57de9a96b6f8e8fce4114856483e6df9c
pbialecki_augeofi <pbialecki@augeofi.com>
2020-02-05 20:04:43 -0600

    Initial sample code for Java lists.

---
+++ lists/src/com/peteadambialecki/Main.java
@@ -0,0 +1,23 @@
+package com.peteadambialecki;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> groceryLine = new ArrayList<>();
+        groceryLine.add("Jerome");
+        groceryLine.add("Beth");
+        groceryLine.add("Sam");
+
+        System.out.println(groceryLine);
+
+        groceryLine.remove(  1);
+
+        System.out.println(groceryLine);
+
+        String jerome = groceryLine.get(0);
+        System.out.println(jerome);
+    }
+}
