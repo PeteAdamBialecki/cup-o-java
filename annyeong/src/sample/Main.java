c5e3ecbf02ce6517e46476022445239b1dbfae27
pbialecki_augeofi <pbialecki@augeofi.com>
2020-02-10 20:27:58 -0600

    FXML refactoring.

--- annyeong/src/sample/Main.java
+++ annyeong/src/sample/Main.java
@@ -18,26 +18,26 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Group root = new Group();
-        Text txt = new Text("Annyeong!");
-        txt.setFont(new Font("Georgia", 80));
-        Label label = new Label("Name: ");
-        TextField nameFld = new TextField();
-        GridPane grid = new GridPane();
-        grid.add(label, 0, 0);
-        grid.add(nameFld, 1, 0);
-        grid.setHgap(20);
-        Button btn = new Button();
-        grid.add(btn, 1, 1);
-        // grid.setGridLinesVisible(true);
-        btn.setText("Say, '안녕!'");
-        btn.setOnAction(evt -> System.out.printf("Annyeong %s!%n",
-                nameFld.getText()));
-        txt.setY(50);
-        VBox box = new VBox();
-        box.getChildren().addAll(txt, grid);
-        root.getChildren().add(box);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        Group root = new Group();
+//        Text txt = new Text("Annyeong!");
+//        txt.setFont(new Font("Georgia", 80));
+//        Label label = new Label("Name: ");
+//        TextField nameFld = new TextField();
+//        GridPane grid = new GridPane();
+//        grid.add(label, 0, 0);
+//        grid.add(nameFld, 1, 0);
+//        grid.setHgap(20);
+//        Button btn = new Button();
+//        grid.add(btn, 1, 1);
+//        // grid.setGridLinesVisible(true);
+//        btn.setText("Say, '안녕!'");
+//        btn.setOnAction(evt -> System.out.printf("Annyeong %s!%n",
+//                nameFld.getText()));
+//        txt.setY(50);
+//        VBox box = new VBox();
+//        box.getChildren().addAll(txt, grid);
+//        root.getChildren().add(box);
         primaryStage.setTitle("Annyeong");
         primaryStage.setScene(new Scene(root, 500, 275));
         primaryStage.show();
