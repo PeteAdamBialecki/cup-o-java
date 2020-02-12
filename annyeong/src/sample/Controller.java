c8447b44a7d1ad64b4dfa574b600a951b6354c9a
pbialecki_augeofi <pbialecki@augeofi.com>
2020-02-11 20:04:45 -0600

    Initial controller set up.

--- annyeong/src/sample/Controller.java
+++ annyeong/src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML private TextField firstName;
+
+    public void howToSayHelloInKorean(ActionEvent actionEvent) {
+        System.out.printf("안녕 %s! %n", firstName.getText());
+    }
 }
