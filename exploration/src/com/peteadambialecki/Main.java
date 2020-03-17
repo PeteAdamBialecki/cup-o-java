faf527ec8c58a1781e365eb5e3689b429fffb746
pbialecki_augeofi <pbialecki@augeofi.com>
2020-03-16 20:26:22 -0500

    Sample function shape.

--- exploration/src/com/peteadambialecki/Main.java
+++ exploration/src/com/peteadambialecki/Main.java
@@ -5,12 +5,19 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import javax.crypto.spec.PSource;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public class Main {
 
     public static void yell(String words) {
+        Objects.requireNonNull(words, () -> "Created issue" + Main.createIssue());
         System.out.printf("%s!!!!! %n", words.toUpperCase());
+    }
+
+    private static String createIssue() {
+        System.out.println("Some external API call to a bug tracker");
+        return "#ABC123";
     }
 
     public static void main(String[] args) {
@@ -26,5 +33,6 @@ public class Main {
 	    Main.yell("But I want the cake");
         ingredients.forEach(System.out::println);
         ingredients.forEach(Main::yell);
+        Main.yell(null);
     }
 }
